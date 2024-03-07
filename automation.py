27956567e1d22372a9b048bb2f87d8d65e1603ed
dbaeza <daniil_iisus@icloud.com>
2024-03-06 20:07:56 -0600

    automate diagnostics and task running

---
+++ automation.py
@@ -0,0 +1,109 @@
+import os
+import subprocess
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+import importlib.metadata as metadata
+
+import pandas as pd
+
+
+class CommandLineRunner(Enum):
+    PYTHON = 'python3'
+    EXECUTABLE = ''
+    SHELL = 'sh'
+
+    @classmethod
+    def get_runner_for_extension(cls, extension: str) -> Enum:
+        if extension == '.py':
+            return cls.PYTHON
+        elif extension == '.sh':
+            return cls.SHELL
+        return cls.EXECUTABLE
+
+
+@dataclass
+class PipelineTask:
+    runner: Enum
+    file_path: Path
+    name: str
+
+
+class AutomatedPipeline:
+
+    @classmethod
+    def run_task(cls, task_name: str):
+        try:
+            task = cls.load_task(task_name)
+            os.system(f"{task.runner.value} {task.file_path}")
+        except Exception as e:
+            raise Exception(f"Falied to run Task {task_name}: {e}")
+
+    @classmethod
+    def load_task(cls, task_name: str) -> PipelineTask:
+        task_file, extension = cls.find_task_file(task_name)
+
+        return PipelineTask(name=task_name, file_path=task_file,
+                            runner=CommandLineRunner.get_runner_for_extension(extension))
+
+    @staticmethod
+    def find_task_file(task_name: str, pipeline_dir='steps') -> (Path, str):
+        steps_dir = Path(os.getcwd()) / pipeline_dir
+        for root, dirs, files in os.walk(steps_dir):
+            for file in files:
+                file_name, extension = os.path.splitext(file)
+                if file_name == task_name:
+                    return steps_dir / file, extension
+
+
+class DependencyChecker:
+
+    @staticmethod
+    def _get_module_versions():
+        installed_packages = metadata.distributions()
+        package_names = []
+        package_versions = []
+
+        for package in installed_packages:
+            package_names.append(package.metadata['Name'])
+            package_versions.append(package.version)
+            print(f"{package.metadata['Name']}=={package.version}")
+
+        return pd.DataFrame({'Name': package_names, 'Version': package_versions})
+
+    @staticmethod
+    def _check_for_updates():
+        try:
+            result = subprocess.run([sys.executable, '-m', 'pip', 'list', '--outdated'],
+                                    capture_output=True, text=True, check=True)
+            outdated_packages = result.stdout.strip().split('\n')[2:]
+            package_names = []
+            package_latest_versions = []
+            if outdated_packages:
+                print("Outdated packages:")
+                for package_info in outdated_packages:
+                    package_name, current_version, latest_version, *_ = package_info.split()
+                    package_names.append(package_name)
+                    package_latest_versions.append(latest_version)
+                    print(f"{package_name}: {current_version} -> {latest_version}")
+            else:
+                print("All packages are up to date.")
+            return pd.DataFrame({'Name': package_names, 'Latest_Version': package_latest_versions})
+        except subprocess.CalledProcessError as e:
+            print("Error:", e)
+
+    @classmethod
+    def get_dependency_check(cls):
+        current_df = cls._get_module_versions()
+        latest_df = cls._check_for_updates()
+        merged_df = pd.merge(current_df, latest_df, on='Name', how='left')
+        merged_df['Latest_Version'] = merged_df['Latest_Version'].fillna(merged_df['Version'])
+        return merged_df
+
+    @classmethod
+    def write_dependency_report(cls, directory: Path) -> Path:
+        dependencies = cls.get_dependency_check()
+        file_full_path = directory / 'dependencyreport.txt'
+        dependencies.to_csv(file_full_path, sep='\t', index=False)
+        return file_full_path
---
+++ steps/diagnostics.py
@@ -0,0 +1,91 @@
+import logging
+import timeit
+from pathlib import Path
+from typing import Iterable
+from automation import AutomatedPipeline, DependencyChecker
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from steps.training import load_model_data
+from steps.scoring import load_model
+from config import ConfigLoader
+from dataclasses import dataclass
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+diagnostics_logger = logging.getLogger()
+
+
+@dataclass
+class ColumnDiagnosticsSummary:
+    stddev: float
+    col_name: str
+    media: float
+    mean: float
+
+
+def model_predictions(logger, model: LogisticRegression, x_df: pd.DataFrame) -> pd.Series:
+    logger.info("Calculating predictions for deployed model.")
+    predictions = model.predict(x_df)
+    return predictions
+
+
+def dataframe_summary(logger, x_df: pd.DataFrame) -> Iterable[ColumnDiagnosticsSummary]:
+    logger.info("Calculating diagnostic summary of the dataset.")
+    stddev = x_df.std()
+    media = x_df.median()
+    mean = x_df.mean()
+    diagnostics = []
+    for col_name in x_df.columns:
+        diagnostics.append(ColumnDiagnosticsSummary(col_name=col_name,
+                                                    stddev=stddev[col_name],
+                                                    media=media[col_name],
+                                                    mean=mean[col_name]))
+    return diagnostics
+
+
+def missing_data(logger, x_df: pd.DataFrame) -> dict:
+    logger.info('Recording missing data.')
+    missing = x_df.isna().sum()
+    n_data = x_df.shape[0]
+    missing = missing / n_data
+    return missing.to_dict()
+
+
+def execution_time(logger):
+    logger.info('Timing ingestion and training pipeline steps')
+    start_time = timeit.default_timer()
+    AutomatedPipeline.run_task("ingestion")
+    ingestion_timing = timeit.default_timer() - start_time
+
+    start_time = timeit.default_timer()
+    AutomatedPipeline.run_task("training")
+    training_timing = timeit.default_timer() - start_time
+
+    return [ingestion_timing, training_timing]
+
+
+def outdated_packages_list(logger, directory: Path):
+    logger.info('Generating dependencies report')
+    file_full_path = directory
+    try:
+        file_full_path = DependencyChecker.write_dependency_report(directory)
+    except Exception as e:
+        logger.error(f"Failed to write dependency report to {file_full_path}: \n"
+                     f"{e}")
+    logger.info(f'Report written successfully at {file_full_path}')
+
+
+def run():
+    config = ConfigLoader.init_from_json_file(diagnostics_logger)
+    test_x, test_y = load_model_data(diagnostics_logger, config.test_data_path, file_name='testdata.csv')
+    model = load_model(diagnostics_logger, config.output_model_path)
+    preds = model_predictions(diagnostics_logger, model, test_x)
+    stats_x, stats_y = load_model_data(diagnostics_logger, config.output_folder_path)
+    diagnositcs_summ = dataframe_summary(diagnostics_logger, stats_x)
+    miss_data = missing_data(diagnostics_logger, stats_x)
+    times = execution_time(diagnostics_logger)
+    outdated_packages_list(diagnostics_logger, config.prod_deployment_path)
+    print(preds, diagnositcs_summ, miss_data, times)
+
+
+if __name__ == '__main__':
+    run()
